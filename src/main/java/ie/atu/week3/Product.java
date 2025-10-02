40d0b7db2b1358e0aa3fc23cc7ecfc53b3c78256
Cathal Burke <g00413483@atu.ie>
2025-10-02 12:28:19 +0100

    Made a list to hold products and added an endpoint to put a product into it

---
+++ src/main/java/ie/atu/week3/Product.java
@@ -0,0 +1,14 @@
+package ie.atu.week3;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+    private String ProductName;
+    private double ProductPrice;
+
+
+
+}
