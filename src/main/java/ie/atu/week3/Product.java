9cefd85de679fa0176218cee5525a4afc5f297b7
Cathal Burke <g00413483@atu.ie>
2025-10-03 18:00:05 +0100

    added basic Validation @NotBlank on name, @NotNull and @Positive on price, updated controller to use @Valid on @RequestBody's

--- src/main/java/ie/atu/week3/Product.java
+++ src/main/java/ie/atu/week3/Product.java
@@ -1,12 +1,18 @@
 package ie.atu.week3;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class Product {
+    @NotBlank
     private String ProductName;
+    @Positive
+    @NotNull
     private double ProductPrice;
 
 
